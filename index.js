e60f0e3d40bb704c7d64f8b76166057d77027249
Labib Ahmed Rasel <rasellabib900@gmail.com>
2024-01-29 19:32:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,117 @@
+// // function rasel() {
+// //   let result = 5;
+// //   for (let i = 5; i <= 12; i++) {
+// //     result = result * i;
+// //   }
+// //   return result;
+// // }
+
+// // const last = rasel();
+
+// // console.log(last);
+
+// //
+
+// // function something() {
+// //   let result = 1;
+// //   for (let i = 5; i <= 12; i++) {
+// //     result = result * i;
+// //   }
+// //   return result;
+// // }
+
+// // const view = something();
+
+// // console.log(view);
+
+// // function moin() {
+// //   let moin = 1;
+// //   for (let a = 1; a <= 12; a++) {
+// //     moin = moin * a;
+// //   }
+// //   return moin;
+// // }
+// // const last = moin();
+// // console.log(last);
+
+// // for (let a = -100; a >= 1; a--) {
+// //   console.log(a);
+// // }
+
+// // function con(str) {
+// //   let count = 0;
+// //   for (const char of str) {
+// //     if (
+// //       char === "a" ||
+// //       char === "e" ||
+// //       char === "i" ||
+// //       char === "o" ||
+// //       char === "u"
+// //     ) {
+// //       count++;
+// //     }
+// //   }
+// //   console.log(count);
+// // }
+
+// // con("rasel");
+// // console.log(con);
+
+// // function rasel(param1) {
+// //   console.log("my name is ", param1);
+// // }
+
+// // rasel("labib ahmmed  rasel");
+// // console.log(rasel);
+
+// function rasel(a, b) {
+//   let result = a + b;
+
+//   return result;
+// }
+// const out1 = rasel(10, 12);
+// const out2 = rasel(20, 2);
+
+// console.log(out1);
+// console.log(out2);
+
+// // 5 + 6 + 7 + 8 + 9 + 19;
+
+// // function moin() {
+// //   let rasel = 1;
+// //   for (let i = 5; i <= 12; i++) {
+// //     rasel = rasel + i;
+// //   }
+// //   return rasel;
+// // }
+
+// // const done = moin();
+// // console.log(done);
+
+// function mind(a, b) {
+//   console.log(a + "sleep" + b);
+// }
+// mind("rasel", "10pm");
+// mind("rasel", "2am");
+// mind("rasel", "1am");
+
+// for (let i = 5; i <= 12; i++) {
+//   console.log(i);
+// }
+
+// let
+// for (let index = 0; index < array.length; index++) {
+//   const index = index ;
+// }
+
+// function name(params) {
+
+// }
+
+function generateMultiplicationTable(number) {
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${i} x ${number} = ${i * number}`);
+  }
+}
+
+generateMultiplicationTable(5); // Output:
